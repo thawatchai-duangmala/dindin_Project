9b94217294e8058b0ea4e2a6e28ecb5c488b60ff
thawatchai-duangmala <6531501058@lamduan.mfu.ac.th>
2025-05-30 20:22:37 +0700

    Update home.dart

--- home.dart
+++ home.dart
@@ -1,4 +1,5 @@
-import 'package:dintalk/user/Homepage/search.dart';
+import 'package:dintalk/user/Homepage/cetegories.dart';
+import 'package:dintalk/user/Homepage/post.dart';
 import 'package:flutter/material.dart';
 
 class HomeUser extends StatefulWidget {
@@ -9,63 +10,159 @@ class HomeUser extends StatefulWidget {
 }
 
 class _HomeUserState extends State<HomeUser> {
-  // bool isSearchClicked = false;
-  // final TextEditingController _searchController = TextEditingController();
+  // search bar
+  bool isSearchClicked = false;
+  final TextEditingController _searchController = TextEditingController();
+
+  // categories
+  final categories = [
+    {'Catid': 1, 'Name': 'General'},
+    {'Catid': 2, 'Name': 'Education'},
+    {'Catid': 3, 'Name': 'Health & beauty'},
+    {'Catid': 4, 'Name': 'Love & relationship'},
+    {'Catid': 5, 'Name': 'Arts'},
+    {'Catid': 6, 'Name': 'Hobby'},
+    {'Catid': 7, 'Name': 'Market'},
+  ];
+
+  // post info
+  final List<Map<String, dynamic>> posts = [
+    {
+      "postId": 101,
+      "Pfimage": "assets/images/384959.png",
+      "text":
+          "Clock tower is very nice place forever Clock tower is very nice place forever",
+      "image": "assets/images/clock-tower.jpg",
+      'Catid': 1,
+      'Name': 'General'
+    },
+    {
+      "postId": 102,
+      "Pfimage": "assets/images/384959.png",
+      "text": "No.01",
+      "image": "assets/images/clock-tower.jpg",
+      'Catid': 1,
+      'Name': 'Arts'
+    },
+    {
+      "postId": 103,
+      "Pfimage": "assets/images/384959.png",
+      "text": "No.02",
+      "image": "assets/images/tomato.PNG",
+      'Catid': 1,
+      'Name': 'Arts'
+    },
+    {
+      "postId": 103,
+      "Pfimage": "assets/images/384959.png",
+      "text": "No.02",
+      "image": "assets/images/salad.jpg",
+      'Catid': 1,
+      'Name': 'Love & relationship'
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        flexibleSpace: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 10, right: 10),
-            child: Row(
-              children: [
-                // categories
-                Expanded(
-                    child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      TextButton(
-                        onPressed: () {},
-                        child: const Text('Textcategories'),
+        backgroundColor: Colors.white,
+        title: Row(
+          children: [
+            Expanded(
+              // Sreach bar
+              child: isSearchClicked
+                  ? Container(
+                      height: 40,
+                      child: TextField(
+                        controller: _searchController,
+                        onChanged: (context) {},
+                        decoration: const InputDecoration(
+                          hintStyle: TextStyle(color: Colors.grey),
+                          border: InputBorder.none,
+                          hintText: "Search. . .",
+                        ),
                       ),
-                      TextButton(
-                        onPressed: () {},
-                        child: const Text('Textcategories'),
+                    )
+                  // show catergories
+                  : SafeArea(
+                      child: SizedBox(
+                        height: 50,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: categories.length,
+                          itemBuilder: (context, index) {
+                            final datacat = categories[index];
+                            return Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: Categories(
+                                catId: datacat['Catid'] as int,
+                                categoryName: datacat['Name'] as String,
+                              ),
+                            );
+                          },
+                        ),
                       ),
-                      TextButton(
-                        onPressed: () {},
-                        child: const Text('Textcategories'),
-                      ),
-                      TextButton(
-                        onPressed: () {},
-                        child: const Text('Textcategories'),
-                      ),
-                      TextButton(
-                        onPressed: () {},
-                        child: const Text('Textcategories'),
-                      ),
-                    ],
-                  ),
-                )),
+                    ),
+            ),
+            // Icon for search and cancels
+            IconButton(
+              icon: Icon(isSearchClicked ? Icons.close : Icons.search),
+              color: Colors.black,
+              onPressed: () {
+                setState(() {
+                  isSearchClicked = !isSearchClicked;
+                  if (!isSearchClicked) {
+                    _searchController.clear();
+                  }
+                });
+              },
+            ),
+          ],
+        ),
+      ),
 
-                // search bar
-                IconButton(
-                  icon: const Icon(Icons.search),
-                  color: Colors.black,
-                  onPressed: () {
-                    showSearch(
-                      context: context,
-                      delegate: CustomSearch(),
+      // body in postinfomation
+      body: SingleChildScrollView(
+        child: Container(
+          // bg color behind card
+          color: Colors.white,
+          child: Column(
+            children: [
+              const Divider(),
+              Container(
+                child: ListView.builder(
+                  // shrinkWrap and physics ใช้เพื่อนscrollจนสุดท้าย ก้อปเน็ตมาาาา
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: posts.length,
+                  itemBuilder: (context, index) {
+                    final data = posts[index];
+                    return PostInfo(
+                      postId: data['postId'] as int,
+                      pfImage: data['Pfimage'] as String,
+                      text: data['text'] as String,
+                      image: data['image'] as String,
+                      catId: data['Catid'] as int,
+                      categoryName: data['Name'] as String,
                     );
                   },
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
+
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.amber[600],
+          shape: const CircleBorder(),
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+          )),
     );
   }
 }
