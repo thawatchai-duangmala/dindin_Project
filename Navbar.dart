ce8d5cbe04f147aca246b264c0aa0305e8b4ee6c
thawatchai-duangmala <6531501058@lamduan.mfu.ac.th>
2025-06-16 21:13:30 +0700

    Update Navbar.dart

--- Navbar.dart
+++ Navbar.dart
@@ -12,28 +12,151 @@ class Navbar extends StatefulWidget {
 }
 
 class _NavbarState extends State<Navbar> {
+  bool light0 = true;
+  bool light1 = true;
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 4,
       child: Scaffold(
         appBar: AppBar(
-          title: Container(
-            child: Padding(
-              padding: const EdgeInsets.only(left: 140),
+          title: Center(
+            child: Container(
               child: Image.asset(
-                'assets/images/dindin.png',
+                'assets/images/logo.PNG',
                 width: 40,
               ),
             ),
           ),
           backgroundColor: Colors.white,
+          bottom: const PreferredSize(
+            preferredSize: Size.fromHeight(0),
+            child: Divider(
+              // height: 1,
+              // color: Colors.grey,
+            ),
+          ),
         ),
 
         //slide menu
-        drawer: const Drawer(
+        drawer: Drawer(
           child: Column(
-            children: [],
+            children: [
+              DrawerHeader(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        InkWell(
+                          onTap: () {},
+                          child: const CircleAvatar(
+                            backgroundImage:
+                                AssetImage('assets/images/kira.jpg'),
+                            radius: 33,
+                          ),
+                        ),
+                        const Padding(
+                          padding: const EdgeInsets.only(top: 60, left: 15),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Username'),
+                              Text('65315010XX'),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 25),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TextButton.icon(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.person,
+                            color: Colors.black,
+                          ),
+                          label: const Text(
+                            'Post',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                        TextButton.icon(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.replay_outlined,
+                              color: Colors.black,
+                            ),
+                            label: const Text(
+                              'Response',
+                              style: TextStyle(color: Colors.black),
+                            )),
+                        TextButton.icon(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.bookmark,
+                              color: Colors.black,
+                            ),
+                            label: const Text(
+                              'Bookmark',
+                              style: TextStyle(color: Colors.black),
+                            )),
+                        TextButton.icon(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.settings,
+                              color: Colors.black,
+                            ),
+                            label: const Text(
+                              'Setting',
+                              style: TextStyle(color: Colors.black),
+                            )),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 12),
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.light_mode,
+                                color: light1 ? Colors.grey : Colors.amber,
+                              ),
+                              Switch(
+                                value: light1,
+                                onChanged: (bool value) {
+                                  setState(() {
+                                    light1 = value;
+                                  });
+                                },
+                              ),
+                              Icon(
+                                Icons.dark_mode,
+                                color: light1
+                                    ? Colors.deepPurpleAccent[800]
+                                    : Colors.grey,
+                              ),
+                            ],
+                          ),
+                        ),
+                        TextButton.icon(
+                            onPressed: () {},
+                            icon: const Icon(Icons.logout_outlined,
+                                color: Colors.red),
+                            label: const Text('Logout',
+                                style: TextStyle(color: Colors.red))),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
 
